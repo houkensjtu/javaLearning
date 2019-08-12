cc8cf5d74983b99f36949473811386d6640994c9
houkensjtu <houkensjtu@gmail.com>
2019-08-11 21:39:08 -0400

    Lecture1 round2.

--- Lecture1/examples/HelloWorld.java
+++ Lecture1/examples/HelloWorld.java
@@ -1,5 +1,7 @@
-class HelloWorld {
-    public static void main (String[] pigs){
+public class HelloWorld 
+{
+    public static void main (String pigs[])
+    {
         System.out.println("Hello world!\n");
     }
 }
